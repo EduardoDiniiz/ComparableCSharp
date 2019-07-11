1182fec975e17906193c3aea6066e73b1dfd17d0
EduardoDiniiz <eduardocco11@gmail.com>
2019-07-11 15:34:59 -0300

    Interface Comparable

---
+++ ComparableCSharp/Program.cs
@@ -0,0 +1,37 @@
+﻿using ComparableCSharp.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ComparableCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\src\file1.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Employee> list = new List<Employee>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+                    list.Sort();
+                    foreach (Employee employee in list)
+                    {
+                        Console.WriteLine(employee);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
